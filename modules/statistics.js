d65c330c95ddbd8ebd0dd35bcb8e1c591696c271
Acsbeni <Acsbenjamin@turr.hu>
2025-10-13 22:00:17 +0200

    stats

--- modules/statistics.js
+++ modules/statistics.js
@@ -4,13 +4,20 @@ const pool = require('../utils/database')
 
 // GET statistics
 
-router.get('/', (req,res) => {
-    
-   pool.query('SELECT(SELECT COUNT(DISTINCT vevo) as userCount FROM forgalom) AS userCount, (SELECT COUNT(DISTINCT termek) FROM forgalom) as productsCount, (SELECT SUM(mennyiseg)from forgalom) as mennyiseg', (error, results, fields) => {
-        if (error) return res.status(500).json({error: error.message})
-        res.status(200).json(results[0],results[1])
-      });
+router.get('/', (req, res) => {
+    pool.query(`SELECT COUNT(DISTINCT vevo) AS 'customersCount', COUNT(DISTINCT termek) AS 'productsCount', SUM(nettoar*mennyiseg) as 'priceSum', SUM(mennyiseg) as salesSum FROM forgalom`, (error, sumResults) => {
+        if (error) return res.status(500).json({ msg: 'Hiba történt az adatbázis lekérdezése közben.'});
+        pool.query(`SELECT termek, sum(mennyiseg) as 'count', sum(nettoar*mennyiseg) as 'price' FROM forgalom WHERE 1 GROUP BY termek;`, (error, productResults) => {
+            sumResults = JSON.parse(JSON.stringify(sumResults))
+            let ellenorzes = {"usersCount": sumResults[0].customersCount,
+                "productsCount": sumResults[0].productsCount,
+                "salesSum": sumResults[0].salesSum,
+                "priceSum": sumResults[0].priceSum,
+                "products": productResults
+            }
+            res.send(ellenorzes)
+        })
+   
     })
-
-
+})
 module.exports = router
